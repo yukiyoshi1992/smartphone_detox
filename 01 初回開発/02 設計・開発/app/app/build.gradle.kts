356e4470ef0c4b40d0213a3594e5581695302d9b
Yukisyoshi <yukiyoshi1992@gmail.com>
2026-06-25 11:08:05 +0900

    compileSdk/targetSdkを37に更新
    androidx.core-ktx 1.19.0等の依存ライブラリがcompileSdk 37以上を要求して
    おりビルドエラーになっていたため対応。

--- 01 初回開発/02 設計・開発/app/app/build.gradle.kts
+++ 01 初回開発/02 設計・開発/app/app/build.gradle.kts
@@ -6,15 +6,13 @@ plugins {
 android {
     namespace = "com.yukiyoshi.smphdetox"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.yukiyoshi.smphdetox"
         minSdk = 34
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
